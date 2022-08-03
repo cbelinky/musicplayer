35ebcc82754339f99e533baba210d2633c9234da
Charles Belinky <cbelinky@terpmail.umd.edu>
2022-08-03 13:17:38 -0400

    initial commit for Java based music player with GUI

---
+++ musicPlayer/src/main/java/com/cbelinky/musicplayer/MusicPlayer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.cbelinky.musicplayer;
+
+/**
+ *
+ * @author Charlie
+ */
+public class MusicPlayer {
+
+    public static void main(String[] args) {
+        MusicForm musicPlayerForm = new MusicForm();
+        musicPlayerForm.setVisible(true);
+        musicPlayerForm.setResizable(false);
+        musicPlayerForm.setLocationRelativeTo(null);
+    }
+}
