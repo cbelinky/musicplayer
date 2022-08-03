192f2d410d250793620bf8e6c418cf7f986c74af
Charles Belinky <cbelinky@terpmail.umd.edu>
2022-08-03 13:25:55 -0400

    initial commit for Java based music player with GUI

---
+++ mp3Player/src/mp3player/Mp3Player.java
@@ -0,0 +1,16 @@
+package mp3player;
+
+/**
+ *
+ * @author Charlie
+ */
+public class Mp3Player {
+
+    public static void main(String[] args) {
+        MusicForm musicPlayerForm = new MusicForm();
+        musicPlayerForm.setVisible(true);
+        musicPlayerForm.setResizable(false);
+        musicPlayerForm.setLocationRelativeTo(null);
+    }
+    
+}
